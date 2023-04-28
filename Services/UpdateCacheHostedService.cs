b7b0125549c02b760384e1395eed6850a7c1106a
Nick Sevens <nick.sevens@delaware.pro>
2023-04-28 15:06:42 +0200

    change while loop

--- Services/UpdateCacheHostedService.cs
+++ Services/UpdateCacheHostedService.cs
@@ -1,38 +1,39 @@
 ﻿namespace Demo.AutoRefreshCache.Services
 {
-	public class UpdateCacheHostedService : BackgroundService
-	{
-		private int _executionCount = 0;
-		private readonly IDataService _dataService;
-		private readonly ILogger<UpdateCacheHostedService> _logger;
+    public class UpdateCacheHostedService : BackgroundService
+    {
+        private int _executionCount = 0;
+        private readonly IDataService _dataService;
+        private readonly ILogger<UpdateCacheHostedService> _logger;
 
-		public UpdateCacheHostedService(IDataService dataService, ILogger<UpdateCacheHostedService> logger)
-		{
-			_dataService = dataService;
-			_logger = logger;
-		}
+        public UpdateCacheHostedService(IDataService dataService, ILogger<UpdateCacheHostedService> logger)
+        {
+            _dataService = dataService;
+            _logger = logger;
+        }
 
-		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-		{
-			using var timer = new PeriodicTimer(TimeSpan.FromSeconds(10));
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using var timer = new PeriodicTimer(TimeSpan.FromSeconds(10));
 
-			while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
-			{
-				try
-				{
-					_dataService.UpdateData();
+            do
+            {
+                try
+                {
+                    _dataService.UpdateData();
 
-					_executionCount++;
-					_logger.LogInformation("Executed PeriodicHostedService - Count: {ExecutionCount}", _executionCount);
-				}
-				catch (Exception ex)
-				{
-					_logger.LogInformation(
-							"Failed to execute UpdateCacheHostedService with exception message '{Message}'. Good luck next round!",
-							ex.Message
-					);
-				}
-			}
-		}
-	}
+                    _executionCount++;
+                    _logger.LogInformation("Executed UpdateCacheHostedService - Count: {ExecutionCount}", _executionCount);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogInformation(
+                            "Failed to execute UpdateCacheHostedService with exception message '{Message}'. Good luck next round!",
+                            ex.Message
+                    );
+                }
+            }
+            while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken));
+        }
+    }
 }
